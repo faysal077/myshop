44da2fa40170a6d97e76d96a23becad6912b8f5d
faysal077 <shadmanfaysal053@gmail.com>
2020-11-26 22:45:55 +0600

    Django 3 by Example Chapter 07

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,9 +27,9 @@ SECRET_KEY = '(!^bu_*k16ryp#ek@vyk+l*!&rd8h8t$u=*7#4(jd=#q#ordng'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
-
+ALLOWED_HOSTS = ['songsoptok.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,6 +121,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_HTTPONLY = True
+
+SESSION_COOKIE_HTTPONLY = True
+
+SECURE_SSL_REDIRECT = True
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user':lambda u:reverse_lazy('user_detail',args=[u.username])
+}
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
