465ec88e8939ed92864df5b3ef251b2a13bfaf18
faysal077 <shadmanfaysal053@gmail.com>
2020-11-27 23:03:29 +0600

    Django 3 by Example Chapter 07

--- myshop/settings.py
+++ myshop/settings.py
@@ -138,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
