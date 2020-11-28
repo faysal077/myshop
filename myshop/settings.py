c63ce2df414ab18d2d948c6eb9920d0906da24b7
faysal077 <shadmanfaysal053@gmail.com>
2020-11-28 12:59:46 +0600

    Django 3 by Example Chapter 07

--- myshop/settings.py
+++ myshop/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,7 +135,7 @@ SESSION_COOKIE_HTTPONLY = True
 SECURE_SSL_REDIRECT = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -143,9 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 
-STATICFILES_DIRS = (
+"""STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)"""
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -153,3 +153,10 @@ EMAIL_HOST_USER = 'bravolulu07@gmail.com'
 EMAIL_HOST_PASSWORD = '@12345@Bangladesh#'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
